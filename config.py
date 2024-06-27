612ba18528497255345247b1ac7fcc2e9f7cf516
Rahulm9508 <153660317+Rahulm9508@users.noreply.github.com>
2024-06-27 17:12:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 api_id = 29707621
 api_hash = "a1f31eeb31a0b9e36f551da6b8b85752"
 bot_token = "7353771619:AAEBS7qRt0WSHPJOFFzP5B9iwqXG0PLoHoo"
-auth_users = "7491167754, 1406300051, 6801378994, 870101201"
+auth_users = "7491167754, 2085612832, 6801378994, 870101201"
 sudo_users = [int(num) for num in auth_users.split(",")]
 osowner_users = "7491167754"
 owner_users = [int(num) for num in osowner_users.split(",")]
